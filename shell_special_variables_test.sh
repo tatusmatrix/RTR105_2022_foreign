a8554e97964a012c5c91bec47830ca373e057d25
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-21 15:54:48 +0300

    Class_05

---
+++ shell_special_variables_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+# example was taken from:
+# https://www.tutorialspoint.com/unix/unix-special-variables.htm
