be527c06dbc3266ed7b2390f1e71c8e1d7f24992
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-28 13:51:30 +0300

    Class_06

---
+++ Class_06/first.c
@@ -0,0 +1,21 @@
+// comment of one row
+// comment of other row
+
+/*
+commenting a paragraph or code fragment
+...
+...
+...
+*/
+
+int main()// for row comment there is only // symbols in begin of comment
+ {
+ 
+ /*
+ if(...){...}
+ */
+
+ //return 100;
+
+ return 0;
+ }
